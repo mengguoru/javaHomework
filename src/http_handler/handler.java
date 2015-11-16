dc2c8cb817213fad6db005c4897a00d3aaf28fc3
hhuysqt <hyq9606@126.com>
2015-11-16 23:52:56 +0800

    HttpServlet handler

---
+++ src/http_handler/handler.java
@@ -0,0 +1,19 @@
+package http_handler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import DA.*;
+
+public class handler extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		super.service(req, resp);
+		// TODO: add handler segments
+	}
+
+}
