afc14e27098595eaf2f745741b83ea445e65bade
hhuysqt <hyq9606@126.com>
2015-11-16 23:52:17 +0800

    Information node form database

---
+++ src/DA/DBInfo.java
@@ -0,0 +1,41 @@
+package DA;
+
+public class DBInfo {
+	String time;
+	String title;
+	String editor;
+	String detail;
+	
+	public DBInfo(String time, String title, String editor, String detail) {
+		super();
+		this.time = time;
+		this.title = title;
+		this.editor = editor;
+		this.detail = detail;
+	}
+
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getEditor() {
+		return editor;
+	}
+	public void setEditor(String editor) {
+		this.editor = editor;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+}
